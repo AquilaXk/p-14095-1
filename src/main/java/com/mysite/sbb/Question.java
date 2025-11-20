411847418f83564c13ddc2da1303f9fb88282826
AquilaXk <illusiveman7@gmail.com>
2025-11-20 14:24:09 +0900

    19강

--- src/main/java/com/mysite/sbb/Question.java
+++ src/main/java/com/mysite/sbb/Question.java
@@ -1,16 +1,18 @@
 package com.mysite.sbb;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
+@Setter
+@Getter
 public class Question {
 
     @Id
@@ -24,4 +26,7 @@ public class Question {
 
     @Column(columnDefinition = "TEXT")
     private String content;
+
+    @OneToMany(mappedBy = "question", cascade =  CascadeType.REMOVE) // OneToMany를 사용할 때 는 변수명 적어야한다
+    private List<Answer> answers;
 }
