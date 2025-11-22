d9fd0aea271192ba1eb0c931fd29492c9e4d8e27
AquilaXk <illusiveman7@gmail.com>
2025-11-22 23:58:13 +0900

    2-11 완료

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,23 +5,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@Controller
+// URL prefix 설정
+@RequestMapping("/question")
 @RequiredArgsConstructor
+@Controller
 public class QuestionController {
     private final QuestionRepository questionRepository;
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Question> questionList = this.questionRepository.findAll();
         model.addAttribute("questionList", questionList);
 
         return "question_list";
     }
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
